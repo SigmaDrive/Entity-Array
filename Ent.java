5975060ba8e8b082a4e3b30f0f6e2cb9f3685f7c
SigmaDrive <sebae97@gmail.com>
2014-11-09 17:04:10 -0500

    Update Ent.java

--- Ent.java
+++ Ent.java
@@ -1,8 +1,10 @@
+//Entity class
+
 public class Ent
 {
   public Ent(long ent_seed)
   {
-    public int aggro = (int)(ent_seed / 1000000000);
+    public int aggro = (int)(ent_seed / 1000000000); // uses first digit in the seed
     public int strength = (int)((ent_seed / 100000000)) % 10; // uses second digit in the seed
     public int defense = (int)((ent_seed / 10000000) % 100); // uses third digit in the seed
   }
