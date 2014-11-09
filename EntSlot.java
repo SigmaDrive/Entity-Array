8fa024863edbe3d0d0f043fa958f2cbca0835b40
SigmaDrive <sebae97@gmail.com>
2014-11-09 17:13:46 -0500

    Create EntSlot.java

---
+++ EntSlot.java
@@ -0,0 +1,9 @@
+//This will be the 'space' that the entities are within, as well as allowing use of a biome system
+
+public class EntSlot
+{
+  public EntSlot(int biome)
+  {
+    int slotBiome = biome;
+  }
+}
